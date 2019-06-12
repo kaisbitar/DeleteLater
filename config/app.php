709426adf305ae733a4f192c272bf49d9faadee2
Kais Bitar <kbitar@dirtt.net>
2019-06-12 16:16:06 -0600

    updating app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'TriggerSampleSoftware'),
+    'name' => env('APP_NAME', 'trigger-sample-software'),
 
     /*
     |--------------------------------------------------------------------------
